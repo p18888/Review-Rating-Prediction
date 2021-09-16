bbb7e2ae989ca40cf9d888721248f1d157c647ed
p18888 <77534300+p18888@users.noreply.github.com>
2021-09-16 21:16:31 +0530

    Update settings.py

--- Rating_Prediction/settings.py
+++ Rating_Prediction/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-n#)!f1@azk%6r#*eplpv8jw+hdhe3x&+7d@0u1$a9=xdzq2mqg'
+SECRET_KEY = 'django-insecure-n#)!f1@azk%6r#*eplpv8jw+hdhe3x&+7d@0u1$a9=xdzq2mqg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
